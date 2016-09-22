dde2e60bcb7b11851d4b6ac6f9328ecb166a6125
henrysteeljr <henrysteel@yahoo.com>
2016-09-22 10:39:22 +0200

    Update UserDetailsContainer.js

--- app/containers/UserDetailsContainer.js
+++ app/containers/UserDetailsContainer.js
@@ -4,19 +4,22 @@ var UserDetails = require('../components/UserDetails');
 var UserDetailsContainer = React.createClass({
  
 
-
-
  componentDidMount : function(){
- console.log(this.props.location.state)
-   
-
-     	
-    },
+ 	console.log(this.props.location.state)
+ 
+    	
+  },
 
 	render: function () {
 		return (
+
+			<UserDetails
 			
-			<UserDetails />
+			Uname={this.props.location.state.Uname}
+			email={this.props.location.state.email}
+			age={this.props.location.state.age}
+			country={this.props.location.state.country} />
+
 		);
 	}
 });
